8e45667b9f633f046fd4cc14feb67dcb9592601d
Rakeshnaik97 <36966210+Rakeshnaik97@users.noreply.github.com>
2024-09-04 08:55:37 -0500

    Update and rename Defender to Defender.ps1

---
+++ Defender.ps1
@@ -0,0 +1,3 @@
+# Get-WinEvent -LogName "Microsoft-Windows-Windows Defender/Operational" | Where-Object { $_.Id -eq 1121} | Format-Table -Wrap
+
+IEdldC1XaW5FdmVudCAtTG9nTmFtZSBNaWNyb3NvZnQtV2luZG93cy1XaW5kb3dzIERlZmVuZGVyL09wZXJhdGlvbmFsIHwgV2hlcmUtT2JqZWN0IHsgdHJ1ZS5JZCAtZXEgMTEyMX0gfCBGb3JtYXQtVGFibGUgLVdyYXA=
